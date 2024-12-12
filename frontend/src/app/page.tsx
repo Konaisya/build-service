02da48fcf2bf65b79de5a7d59fc3c9f9d2be9d6b
axqq <makinfaer@gmail.com>
2024-12-12 19:59:00 +0400

    new sidebar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,24 @@
 "use client"
+import { useState } from 'react';
+import "@/styles/main.css";
+import Image from "next/image";
 
-import "@/styles/main.css"
-import Image from "next/image"
-
-
-export default function Home() {
+const Page = () => {
   return (
+  
     <div className="main-header">
       <div className="main-header-text">
-        <h1 className="main-header-h1"> Строим будущее</h1>
+        <h1 className="main-header-h1">Строим будущее</h1>
         <h1 className="main-header-h1">Надежные решения для вас</h1>
-        <p className="main-header-p">Наша строительная компания предлагает качественные и надежные услуги по возведению и ремонту жилых и коммерческих объектов.</p>
+        <p className="main-header-p">
+          Наша строительная компания предлагает качественные и надежные услуги по возведению и ремонту жилых и коммерческих объектов.
+        </p>
       </div>
-      <Image className="main-header-img" width={50} height={50} src={"/homes.jpg"}  alt="" />
+       
+      {/* <Image className="main-header-img" width={50} height={50} src={"/homes.jpg"} alt="" /> */}
     </div>
+ 
   );
-}
+};
+
+export default Page;
